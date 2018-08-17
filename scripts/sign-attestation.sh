2920e19b01c415306da0b4dbd6fb6b929c8f991c
HRMPW <pwolf@cloudbees.com>
2018-08-16 19:25:11 -0700

    typo in attesting build image

--- scripts/sign-attestation.sh
+++ scripts/sign-attestation.sh
@@ -50,4 +50,4 @@ gcloud beta container binauthz attestations create --artifact-url="${ARTIFACT_UR
 
 echo "Attestation created by Attestor: ${ATTESTOR_ID} for Image: ${ARTIFACT_URL}"
 
-gcloud beta container binauthz attestations list --attestor=${ATTESTOR_ID} --project=${ATTESTOR_PROJECT} --artifact-url=${ARTIFACT_URL}"
+gcloud beta container binauthz attestations list --attestor=${ATTESTOR_ID} --project=${ATTESTOR_PROJECT} --artifact-url=${ARTIFACT_URL}
